de4ef869a63e1e6e6e2b57aaf0a6806bfba7dc2e
mariaagresta <mmontero@agresta.org>
2022-09-21 11:38:52 +0200

    Empiece

--- SCRIPT.R
+++ SCRIPT.R
@@ -1,5 +1,17 @@
 
+
+
 # Aquí vamos a ir escribiendo nuestras cositas
+
+
+library (tidyverse)
+library(here)
+estadillo <- read_csv("datos/FORCIPULAS_TOTAL_H_D.csv")
+glimpse(estadillo)
+
+
+
+
 
 
 
